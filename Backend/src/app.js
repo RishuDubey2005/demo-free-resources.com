5dad8399252453fe881c4bc3edc2fdf5a2d646f5
SUDHANSHU SHEKHAR <148328115+RishuDubey2005@users.noreply.github.com>
2026-03-13 22:21:40 +0530

    Update CORS origin for production environment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://spotify-songs-frontend.onrender.com",
+    "https://demo-free-content-com.onrender.com",
     process.env.FRONTEND_URL
   ],
   credentials: true
